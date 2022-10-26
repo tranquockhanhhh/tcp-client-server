68687cfabcf1d0aedae49bf49a5ae839e55cbf55
Tran Quoc Khanh <91265666+tranquockhanhhh@users.noreply.github.com>
2022-10-26 23:24:14 +0700

    Add files via upload

---
+++ TCPClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+class SimpleTcpSocketClient
+{
+    public static void Main()
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        try
+        {
+            socket.Connect(remoteEP);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Unable to connect to server. ");
+            Console.WriteLine(e);
+            return;
+        }
+
+        byte[] data = new byte[1024];
+        int dataSize = socket.Receive(data);
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, dataSize));
+
+        String input = null;
+        while (true)
+        {
+            Console.Write("Enter Message for Server <Enter to Stop>: ");
+            input = Console.ReadLine();
+            if (input.Length == 0)
+                break;
+            socket.Send(Encoding.ASCII.GetBytes(input));
+            data = new byte[1024];
+            dataSize = socket.Receive(data);
+
+            Console.WriteLine("Echo: " + Encoding.ASCII.GetString(data, 0, dataSize));
+        }
+        Console.WriteLine("Disconnecting from Server..");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+}
---
+++ TCPServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class SimpleTcpSocketServer
+{
+    public static void Main()
+    {
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 9050);
+        server.Bind(localEP);
+        server.Listen(10);
+
+        Console.WriteLine("Waiting for Client...");
+        Socket client = server.Accept();
+        IPAddress clientAddress = ((IPEndPoint)client.RemoteEndPoint).Address;
+        Console.WriteLine("Got connection from " + clientAddress);
+
+        byte[] data = Encoding.ASCII.GetBytes("Welcome to my test server");
+        client.Send(data);
+
+        int dataSize = 0;
+        while (true)
+        {
+            data = new byte[1024];
+            dataSize = client.Receive(data);
+            if (dataSize == 0)
+                break;
+
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, dataSize));
+            client.Send(data, dataSize, SocketFlags.None);
+        }
+        client.Close();
+        server.Close();
+    }
+}
